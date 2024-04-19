c77584900f779721ea45fd1fa9b00418fb47558b
NCP08 <navercloudplatform08.gmail.com>
2024-04-19 11:35:57 +0900

    page 449 QueryDsl을 사용하기 위해 라이브러리 로딩 후 Q도메인 클래스 생성

--- b01/src/main/java/org/zerock/b01/repository/BoardRepository.java
+++ b01/src/main/java/org/zerock/b01/repository/BoardRepository.java
@@ -1,6 +1,9 @@
 package org.zerock.b01.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.zerock.b01.domain.Board;
 
 /* 인터페이스를 만들고
@@ -17,4 +20,13 @@ private BoardRepository boardRepository;
 나 생성자 주입을 통해서 사용하면 된다.
 * */
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    // MariaDB에 적합한 sql
+    @Query(value="select now()", nativeQuery = true)
+    String getTime();
+
+    // JPA에서 정한 표준 SQL, JPA에서 사용하는 Hibernate에서 NativeQuery로 자동 변환해서
+    // DBMS에 전달한다.
+    @Query("select b from Board b where b.title like concat('%',:keyword,'%')")
+    Page<Board> findKeyword(String keyword, Pageable pageable);
 }
